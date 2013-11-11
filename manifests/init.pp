a32d47577102023268571a33cb19c98ec10b4b8b
Lucien Stuker <lucien.stuker@inwork.ch>
2013-11-11 09:01:37 +0100

    Add parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,27 @@
 # This puppet module install git package
 #
 # === Parameters
+#   [*ensure*]
+#     Ensure if present or absent.
+#     Default: present
 #
-# === Variables
+#   [*autoupgrade*]
+#     Upgrade package automatically, if there is a newer version.
+#     Default: false
+#
+#   [*package*]
+#     Name of the package.
+#     Only set this, if your platform is not supported or you know,
+#     what you're doing.
+#     Default: auto-set, platform specific
 #
 # === Examples
 #
 #  class { git:
+#  }
+#
+#  class { git:
+#   autoupgrade => true,
 #  }
 #
 # === Authors
@@ -19,11 +34,30 @@
 #
 # Copyright 2013 Lucien Stuker.
 #
-class git() inherits git::params {
+class git(
+  $ensure              = 'present',
+  $autoupgrade         = false,
+  $package             = $git::params::package,
+) inherits git::params {
 
-  package { 'git':
-    name      => $git_package_name,
-    ensure    => $ensure,
+    case $ensure {
+    /(present)/: {
+      if $autoupgrade == true {
+        $package_ensure = 'latest'
+      } else {
+        $package_ensure = 'present'
+      }
+    }
+    /(absent)/: {
+      $package_ensure = 'absent'
+    }
+    default: {
+      fail('ensure parameter must be present or absent')
+    }
+  }
+
+  package { $package:
+    ensure    => $package_ensure,
   }
 
 }
